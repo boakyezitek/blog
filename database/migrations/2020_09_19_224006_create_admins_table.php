e49d9e694821211c20a9b56d597bccf86862e55c
Boakye51 <53356674+Boakye51@users.noreply.github.com>
2020-09-19 22:50:38 +0000

    Migrations and model created for user and admin.

--- database/migrations/2020_09_19_224006_create_admins_table.php
+++ database/migrations/2020_09_19_224006_create_admins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class CreateAdminsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,13 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 256);
-            $table->string('subtitle', 100);
-            $table->string('slug', 100);
-            $table->text('body');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone');
             $table->boolean('status');
-            $table->integer('posted_by');
-            $table->string('image');
             $table->timestamps();
         });
     }
@@ -33,6 +31,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('admins');
     }
 }
