2844e1df47f080d2bf623dc27ee73036666f368d
Boakye51 <53356674+Boakye51@users.noreply.github.com>
2020-09-19 22:29:47 +0000

    use post view created. Post migration added

---
+++ resources/views/user/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+ @include('user/layout/head')
+</head>
+
+<body>
+
+  <!-- Navigation -->
+ @include('user/layout/header')
+
+ @section('main-content')
+ @show
+
+
+ @include('user/layout/footer');
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('user/blog');
+})->name('blog');
+Route::get('/post', function () {
+    return view('user/post');
+})->name('post');
